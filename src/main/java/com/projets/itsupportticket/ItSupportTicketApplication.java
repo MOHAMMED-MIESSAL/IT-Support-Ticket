a3b2300879f57b224d3f54c73f94f39769360d6d
Mohammed Miessal <mohammedmiessal@gmail.com>
2025-02-22 21:22:20 +0100

    Create and Structure the Spring boot project

---
+++ src/main/java/com/projets/itsupportticket/ItSupportTicketApplication.java
@@ -0,0 +1,13 @@
+package com.projets.itsupportticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSupportTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItSupportTicketApplication.class, args);
+    }
+
+}
