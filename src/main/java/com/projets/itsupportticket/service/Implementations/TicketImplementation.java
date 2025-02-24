5b4e6f977935c1b239ff56caab531d8c08c7c4be
Mohammed Miessal <mohammedmiessal@gmail.com>
2025-02-24 00:49:28 +0000

    Add update status methode to make the IT Support to change the status of the ticket

--- src/main/java/com/projets/itsupportticket/service/Implementations/TicketImplementation.java
+++ src/main/java/com/projets/itsupportticket/service/Implementations/TicketImplementation.java
@@ -1,12 +1,11 @@
 package com.projets.itsupportticket.service.Implementations;
 
 import com.projets.itsupportticket.domain.Ticket;
+import com.projets.itsupportticket.domain.User;
 import com.projets.itsupportticket.enums.Status;
 import com.projets.itsupportticket.exception.CustomValidationException;
 import com.projets.itsupportticket.repository.TicketRepository;
 import com.projets.itsupportticket.service.TicketService;
-import com.projets.itsupportticket.service.UserService;
-import liquibase.exception.CustomChangeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -42,11 +41,10 @@ public class TicketImplementation implements TicketService {
     @Override
     public void delete(UUID id) {
         if (ticketRepository.findById(id).isEmpty()) {
-            throw new RuntimeException("Ticket with id : " + id + " not found");
+            throw new CustomValidationException("Ticket with id : " + id + " not found");
         }
         ticketRepository.deleteById(id);
     }
-
 
     @Override
     public Optional<Ticket> findById(UUID id) {
@@ -56,10 +54,24 @@ public class TicketImplementation implements TicketService {
     @Override
     public Ticket update(UUID id, Ticket ticket) {
         if (ticketRepository.findById(id).isEmpty()) {
-            throw new RuntimeException("Ticket with id : " + id + " not found");
+            throw new CustomValidationException("Ticket with id : " + id + " not found");
         }
         ticket.setId(id);
         ticket.setCreationDate(LocalDateTime.now());
         return ticketRepository.save(ticket);
     }
+
+    @Override
+    public Ticket updateStatus(UUID id, Status newStatus, User changedBy) {
+        Ticket ticket = ticketRepository.findById(id)
+                .orElseThrow(() -> new CustomValidationException("Ticket not found"));
+
+        ticket.setStatus(newStatus);
+
+        // Log the status change is automatically done by the AuditLogAspect
+        // auditLogService.logStatusChange(ticket, oldStatus, newStatus, changedBy);
+
+        return ticketRepository.save(ticket);
+    }
+
 }
